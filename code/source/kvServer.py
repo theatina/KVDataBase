1fbe10aa7baa5625c28789a035245e393a55b614
Theatina <ct.kylafi@gmail.com>
2021-03-12 20:57:36 +0200

    update

--- code/source/kvServer.py
+++ code/source/kvServer.py
@@ -2,6 +2,16 @@ import trie as tr
 
 data_filepath = "../data/"
 
-temp_list = ["theatina", "doyouevenexist", "no", "piano"]
+# temp_list = ["theatina", "doyouevenexist", "no", "piano"]
 
+temp = ["bear", "bell", "bid", "bull", "buy", "sell", "stock", "stop"]
 
+trie_dictionary = tr.trie_node(".")
+
+# for word in temp:
+    # trie_dictionary.insert(word)
+
+trie_dictionary.insert("bear")
+print(trie_dictionary.find_word("bear"))
+
+# trie_dictionary.print_trie()
--- code/source/trie.py
+++ code/source/trie.py
@@ -1,3 +1,61 @@
-class Trie_structure():
+class trie_node():
 
+    def __init__(self,character):
+        self.character = character
+        self.parent_node = None
+        self.children_nodes = [ ]
+        self.end_of_word = False
+        
     
+    # insertion of word in the trie structure
+def insert(self, word):
+    curr_node = self
+    for letter in word:
+        char_in_trie = False
+        for child_node in curr_node.children_nodes:
+            if letter == child_node.character:
+                char_in_trie = True
+                curr_node = child_node
+                break
+        
+        if char_in_trie == False:
+            new_child = trie_node(letter)
+            curr_node.children_nodes.append(new_child)   
+            curr_node = curr_node.children_nodes[-1]
+            
+    curr_node.end_of_word = True
+    
+    print(self.children_nodes)
+
+def find_word(self,word):
+    word_found = False
+    curr_node = self
+    
+    for letter in curr_node.children_nodes:
+        letter_found = False
+        
+        for child_node in curr_node.children_nodes:
+            if letter == child_node.character:
+                print(letter)
+                curr_node=child_node
+                letter_found = True
+                break
+            
+        if letter_found == False:
+            return word_found
+
+    if letter_found == True and curr_node.end_of_word == True:
+        word_found = True
+    
+    return word_found
+
+
+def print_words(self):
+    temp_word = ""
+    end_of_word = False
+    trie_node = self
+
+    # while end_of_word==0:
+        
+
+    # def delete_trie(self):
