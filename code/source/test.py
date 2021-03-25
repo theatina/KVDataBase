ae08076a03e4b00ca403160614c65371b83695d7
Theatina <ct.kylafi@gmail.com>
2021-03-25 19:44:23 +0200

    silly parser

--- code/source/test.py
+++ code/source/test.py
@@ -1,14 +1,19 @@
+import re
+import json
+
 def silly_tokenizer(data):
     temp_data_dict = {}
-    data = data.split(" ")
+    top_level_key,tlk_value = data.split(" : ",maxsplit=1)
+    print(f"\nBefore:\nTLK: {top_level_key}\nValue: {tlk_value}\n")
+    data = tlk_value.split(" ")
     print(data)
-    # exit( )
 
+    return temp_data_dict
 
-    return data
 
 string = '"key1" : { "height" : { "age" : 99 ; "postal_code" : {} } }"'
-print(string)
-data = silly_tokenizer(string)
-print(data)
+
+data_dict = silly_tokenizer(string)
+print(f"\nAfter:\n{data_dict}")
+
 
