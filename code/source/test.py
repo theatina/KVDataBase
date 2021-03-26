8869eb8ef7f6dee872156e6bfc03dd162fc0e880
Theatina <ct.kylafi@gmail.com>
2021-03-26 10:50:36 +0200

    parsing done - json thanks!

--- code/source/test.py
+++ code/source/test.py
@@ -1,36 +1,80 @@
 import re
+import json
 
 def silly_parser(val):
     temp_d = {}
-
+    print(val)
+    if len(val)==1 or val=="{}":  
+        return val
     
+    elif val!="{}":
+        val_tokens = val.split(" ")
+        temp_key = ""
+        
+        pos = 0
+        while pos!=len(val_tokens):
+            if val_tokens[pos]=="{" or val_tokens==";":
+                pos+=1
+                temp_key=val_tokens[pos]
+                pos+=1
+            
+            # elif val_tokens[pos]==":":
+                pos+=1
+                # if val_tokens[pos]=="{":
+                temp_d[temp_key] = silly_parser(" ".join(val_tokens[pos:]))
+                pos+=1
+                # else:
+                    #     temp_d[temp_key] = val_tokens[pos]
+            
+            # elif val_tokens[pos]=="}" and val_tokens[pos-1]!="{":
+            #     return temp_d
+            else:
+                return val_tokens[pos]
+            
+            # pos+=1
 
+            
+                
+        
 
     return temp_d 
 
 
 
-def silly_tokenizer(data):
+def json_to_dict(data):
     temp_data_dict = {}
-    top_level_key,tlk_value = data.split(" : ",maxsplit=1)
-    # print(f"\nBefore:\nTLK: {top_level_key}\nValue: {tlk_value}\n")
-    # val = tlk_value.split(" ")
-    # print(val)
-
-    temp_data_dict[top_level_key] = silly_parser(tlk_value)
-
-
+    print(data)
+    if data!="{}":
+        if ";" in data:
+            data = re.sub(";", ",", data)
+            # print(data)
+        
+        top_level_key,tlk_value = data.split(" : ", maxsplit=1)
+        # print(f"\nBefore:\nTLK: {top_level_key}\nValue: {tlk_value}\n")
+        # val = tlk_value.split(" ")
+        # print(val)
+        # print(tlk_value)
+        temp_data_dict[top_level_key] = json.loads(tlk_value)
 
     return temp_data_dict
 
 
-string = '"key1" : { "height" : { "age" : 99 ; "postal_code" : {} } }'
-string2 = '"key2" : {}'
 
-data_dict = silly_tokenizer(string)
-data_dict2 = silly_tokenizer(string2)
-print(f"\nAfter:\n{data_dict}")
-# print(f"\nAfter:\n{data_dict2}")
+
+# string = '"key1" : { "height" : { "age" : 99 ; "postal_code" : {} } }'
+# data_dict = json.loads( string)
+# print(data_dict["key1"]["height"]["age"])
+# exit()
+
+# string2 = '"key2" : {}'
+
+with open("../data/dataToIndex.txt","r",encoding="utf-8") as r:
+    lines = r.readlines()
+    for s in lines:
+        data_dict = json_to_dict(s)
+        # data_dict2 = json_to_dict(string2)
+        print(f"\nAfter:\n{data_dict}")
+        # print(f"\nAfter:\n{data_dict2}")
 
 
 
